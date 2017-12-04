637b1feeac352ec96e2bf970e6a951ecbe5e88ee
GreegAV <greegav@gmail.com>
2017-12-04 15:27:24 +0200

    Initial commit

---
+++ src/test/java/StringHelperTest.java
@@ -0,0 +1,17 @@
+import org.junit.Assert;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by avg-m on 04/12/2017.
+ */
+public class StringHelperTest {
+
+    @org.junit.Test
+    public void truncateAInFirst2Positions() {
+    }
+
+    @org.junit.Test
+    public void areFirstAndLastTwoCharactersTheSame() {
+    }
+}
