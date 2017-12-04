b8d76b41568ca8a8d2bb469118f61bdfc85170f5
GreegAV <greegav@gmail.com>
2017-12-04 17:32:00 +0200

    @RunWith(Suite.class)

---
+++ src/test/java/SomeClassesTest.java
@@ -0,0 +1,11 @@
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.*;
+
+
+@RunWith(Suite.class)
+@SuiteClasses({ArrayEquals.class,StringHelperTest.class})
+public class SomeClassesTest {
+
+}
