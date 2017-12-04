6fc8c597f66cbf1f609267b8f4a2a8b9cfd3af8b
GreegAV <greegav@gmail.com>
2017-12-04 16:48:46 +0200

    @Test (expected)

--- src/test/java/ArrayEquals.java
+++ src/test/java/ArrayEquals.java
@@ -11,29 +11,47 @@ import static org.junit.Assert.assertEquals;
 public class ArrayEquals {
 
     @Test
-    public void testArrayEquals(){
-        int[] arr = {1,2,3,56,3};
-        int[] expected = {1,2,3,3,56};
+    public void testArrayEquals() {
+        int[] arr = {1, 2, 3, 56, 3};
+        int[] expected = {1, 2, 3, 3, 56};
         Arrays.sort(arr);
-        assertArrayEquals(expected,arr);
+        assertArrayEquals(expected, arr);
     }
 
     @Test
-    public void testArrayEquals_False(){
-        int[] arr = {1,2,3,56,3};
-        int[] expected = {1,2,3,3,56};
+    public void testArrayEquals_False() {
+        int[] arr = {1, 2, 3, 56, 3};
+        int[] expected = {1, 2, 3, 3, 56};
         Arrays.sort(arr);
-        assertEquals(expected,arr);
+        assertEquals(expected, arr);
     }
 
     @Test
-    public void testArrayEquals_False2(){
-        int[] arr = {1,2,3,56,3};
-        int[] expected = {1,2,3,4,56};
+    public void testArrayEquals_False2() {
+        int[] arr = {1, 2, 3, 56, 3};
+        int[] expected = {1, 2, 3, 4, 56};
         Arrays.sort(arr);
-        assertArrayEquals(expected,arr);
+        assertArrayEquals(expected, arr);
     }
 
+    @Test
+    public void testArrayEquals_NPE() {
+        int[] arr = null;
+        int[] expected = {1, 2, 3, 4, 56};
+        try {
+            Arrays.sort(arr);
+        } catch (NullPointerException e) {
+            System.out.println("NPE!");
+        }
+//        assertArrayEquals(expected, arr);
+    }
 
+    @Test(expected = NullPointerException.class)
+    public void testArrayEquals_NPE2() {
+        int[] arr = null;
+        int[] expected = {1, 2, 3, 4, 56};
+            Arrays.sort(arr);
+        assertArrayEquals(expected, arr);
+    }
 
 }
