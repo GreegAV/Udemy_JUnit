75820bcaf0f0f21af1307cd5a35c3fdd00bcf0ab
GreegAV <greegav@gmail.com>
2017-12-04 15:53:38 +0200

    2

--- src/main/java/StringHelper.java
+++ src/main/java/StringHelper.java
@@ -1,7 +1,7 @@
 public class StringHelper {
 
     public static String truncateAInFirst2Positions(String str) {
-        if (str.length() <= 2)
+        if ( str.length() <= 2 )
             return str.replaceAll("A", "");
 
         String first2Chars = str.substring(0, 2);
@@ -13,15 +13,12 @@ public class StringHelper {
 
     public static boolean areFirstAndLastTwoCharactersTheSame(String str) {
 
-        if (str.length() <= 1)
+        if ( str.length() <= 1 )
             return false;
-        if (str.length() == 2)
+        if ( str.length() == 2 )
             return true;
-
         String first2Chars = str.substring(0, 2);
-
         String last2Chars = str.substring(str.length() - 2);
-
         return first2Chars.equals(last2Chars);
     }
 
--- src/test/java/StringHelperTest.java
+++ src/test/java/StringHelperTest.java
@@ -1,17 +1,38 @@
-import org.junit.Assert;
+import org.junit.*;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
 
 /**
  * Created by avg-m on 04/12/2017.
  */
 public class StringHelperTest {
 
-    @org.junit.Test
-    public void truncateAInFirst2Positions() {
+    @Test
+    public void truncateAInFirst2PositionsTest0() {
+        StringHelper stringHelper = new StringHelper();
+        assertEquals("BAXX", stringHelper.truncateAInFirst2Positions("ABAXX"));
+
+        // expected, actual
     }
 
-    @org.junit.Test
-    public void areFirstAndLastTwoCharactersTheSame() {
+    @Test
+    public void truncateAInFirst2PositionsTest2() {
+        StringHelper stringHelper2 = new StringHelper();
+        assertEquals("AXX", stringHelper2.truncateAInFirst2Positions("AAAXX"));
+
+        // expected, actual
     }
+
+    @Test
+    public void truncateAInFirst2PositionsTest3() {
+        StringHelper stringHelper3 = new StringHelper();
+        assertEquals("BCAXX", stringHelper3.truncateAInFirst2Positions("BCAXX"));
+
+        // expected, actual
+    }
+
+//    @Test
+//    public void areFirstAndLastTwoCharactersTheSameTest() {
+//    }
 }
